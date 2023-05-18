23247bb80cec2cb1f54ddba66c82eabea8cb0a70
Connor <cmccaul8@uwo.ca>
2023-05-18 19:16:10 -0400

    Cross Origin method blocked for non-http

--- src/formbuilder-laptop.js
+++ src/formbuilder-laptop.js
@@ -57,9 +57,38 @@ qcchecks.forEach(check => {
     }
 });
 
-const formElement = document.querySelector("form");
-const request = new XMLHttpRequest();
-request.responseType = "JSON";
-request.open("POST", "laptop.html");
-request.send(new FormData(formElement));
-console.log(request);
+window.addEventListener("load", () => {
+    function sendData() {
+      const XHR = new XMLHttpRequest();
+  
+      // Bind the FormData object and the form element
+      const FD = new FormData(form);
+  
+      // Define what happens on successful data submission
+      XHR.addEventListener("load", (event) => {
+        alert(event.target.responseText);
+      });
+  
+      // Define what happens in case of error
+      XHR.addEventListener("error", (event) => {
+        alert("Oops! Something went wrong.");
+      });
+  
+      // Set up our request
+      XHR.open("POST",document);
+  
+      // The data sent is what the user provided in the form
+      XHR.send(FD);
+    }
+  
+    // Get the form element
+    const form = document.getElementById("laptop-qc-form");
+  
+    // Add 'submit' event handler
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+  
+      sendData();
+    });
+  });
+  
