b844a7576906039d3488969631aa09af1b875b9d
Lord-Zer0 <cmccaul8@uwo.ca>
2023-06-30 13:59:50 -0400

    load_db fixes

--- src/formbuilder-database.js
+++ src/formbuilder-database.js
@@ -1,7 +1,7 @@
-const HOST = "http://192.168.1.62:8000"
+const HOST = "http://192.168.1.27:8000"
 
 const netload = document.querySelector("#loadDB");
-netload.addEventListener("Click", load_db);
+netload.addEventListener("Click", load_DB);
 
 function init() {
     // Load the table from db and populate rows
@@ -10,16 +10,21 @@ function init() {
 function search_query() {
 
 }
-
-function load_db() {
-    fetch(HOST + "/api/search", {
-        method: "POST",
-        mode: "no-cors",
-        body: new URLSearchParams({
-          
-        })
-    })
-    .then(response => response.json())
-    .then(text => console.log(text))
-    .catch(error => console.error(error));
+async function load_DB(query, sortby, ascending) {
+    try {
+        const response = await fetch(HOST + "/api/search", {
+            method: "POST",
+            mode: "no-cors",
+            body: new URLSearchParams({
+                "limit": "10",
+                "search": (query != null) ? query : "",
+                "order_table": (sortby != null) ? sortby : "",
+                "ascending": (ascending != null) ? ascending : "",
+            })
+        });
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+  }
 }
