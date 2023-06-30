cdb4ef9a36d80c79a001be4151989a696df83419
Lord-Zer0 <cmccaul8@uwo.ca>
2023-06-30 10:57:25 -0400

    database fetching test

--- src/formbuilder-database.js
+++ src/formbuilder-database.js
@@ -1,3 +1,7 @@
+const HOST = "http://192.168.1.62:8000"
+
+const netload = document.querySelector("#loadDB");
+netload.addEventListener("Click", load_db);
 
 function init() {
     // Load the table from db and populate rows
@@ -5,4 +9,17 @@ function init() {
 
 function search_query() {
 
+}
+
+function load_db() {
+    fetch(HOST + "/api/search", {
+        method: "POST",
+        mode: "no-cors",
+        body: new URLSearchParams({
+          
+        })
+    })
+    .then(response => response.json())
+    .then(text => console.log(text))
+    .catch(error => console.error(error));
 }
